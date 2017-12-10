e5ca97b3eca29f019a0738caa828ada39b949d13
Kim Lindholm <misc@dynevision.com>
2017-12-10 16:41:21 +0700

    Remove example test

--- spec/models/example_model_spec.exs
+++
@@ -1,7 +0,0 @@
-defmodule PhoenixIntercoolerTutorial.ExampleModelSpec do
-  use ESpec.Phoenix, model: PhoenixIntercoolerTutorial.Example, async: true
-
-  it "does something" do
-    expect(true).to be_truthy()
-  end
-end
