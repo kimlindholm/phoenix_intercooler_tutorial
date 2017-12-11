ce741dd75f1fdadd0f3bf006cb9857e643d86251
Kim Lindholm <misc@dynevision.com>
2017-12-11 23:57:02 +0700

    Add ESpec and run "MIX_ENV=test mix espec_phoenix.init"

--- mix.exs
+++ mix.exs
@@ -40,7 +40,12 @@ defmodule PhoenixIntercoolerTutorial.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
+
+      # Code analysis
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      # Tests
+      {:espec_phoenix, "~> 0.6.9", only: :test},
+
       {:cowboy, "~> 1.0"}
     ]
   end
