20214d9e04a3afa7a46bf6b019749ff185f91c56
Kim Lindholm <misc@dynevision.com>
2017-12-13 13:35:48 +0700

    Add Hound custom helpers

--- spec/espec_phoenix_extend.ex
+++ spec/espec_phoenix_extend.ex
@@ -16,6 +16,7 @@ defmodule ESpec.Phoenix.Extend do
       import PhoenixIntercoolerTutorialWeb.Router.Helpers
 
       use Hound.Helpers
+      import PhoenixIntercoolerTutorial.Hound.CustomHelpers
 
       before do
         Hound.start_session()
